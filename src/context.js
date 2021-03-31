c0584f65ac04ecb0ca26b799b91540656764bb63
Svetlana Stoycheva <s.stoycheva@gmail.com>
2021-03-31 17:19:37 +0300

    third commit

--- src/context.js
+++ src/context.js
@@ -43,11 +43,22 @@ const AppProvider = ({ children }) => {
     dispatch({ type: REMOVE_STORY, payload: id })
   }
 
+  const handleSearch = (query) => {
+    dispatch({ type: HANDLE_SEARCH, payload: query })
+  }
+
+  const handlePage = (value) => {
+    // console.log(value)
+    dispatch({ type: HANDLE_PAGE, payload: value })
+  }
+
   useEffect(() => {
     fetchStories(`${API_ENDPOINT}query=${state.query}&page=${state.page}`)
-  }, [])
+  }, [state.query, state.page])
   return (
-    <AppContext.Provider value={{ ...state, removeStory }}>
+    <AppContext.Provider
+      value={{ ...state, removeStory, handleSearch, handlePage }}
+    >
       {children}
     </AppContext.Provider>
   )
